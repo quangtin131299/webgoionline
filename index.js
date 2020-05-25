1be571411cee375a656d8a005f9a99bf1579c1b1
quangtin131299 <63833652+quangtin131299@users.noreply.github.com>
2020-05-25 17:59:37 +0800

    abc

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const io = require('socket.io')(3000);
 
-const arruserinfo =[];
+const arruserinfo = [];
+
+const arrsocket = [];
+
 
 io.on('connection', socket => {
-    socket.on('NGUOI_DUNG_DANG_KY', username =>{
-        arruserinfo.push(username);
-        socket.emit('DANH_SACH_ONLINE',arruserinfo);
-        io.emit('CO_NGUOI_MOI' , username);
+    arrsocket.push(socket);
+    socket.on('NGUOI_DUNG_DANG_KY', username => {
+        let isExist = arruserinfo.some(u => username.ten == u.ten);
+        socket.peerid = username.peerid;
+        if (isExist) {
+            socket.emit('DANG_KY_THAT_BAI', username.ten + ' Đăng ký thất bai');
+        } else {
+            arruserinfo.push(username);
+        }
+        io.sockets.emit('DANH_SACH_ONLINE', arruserinfo);
+
+        // io.sockets.emit('CO_NGUOI_MOI' , username);
+    });
+   
+    if (arruserinfo.length != 0) {
+        io.sockets.emit('DANH_SACH_ONLINE', arruserinfo);
+    }
+
+    socket.on('disconnect', () => {
+    
+        for (let i = 0; i < arrsocket.length; i++) {
+            if (arrsocket[i].connected == false) {
+                let index = arruserinfo.findIndex(user => user.peerid == arrsocket[i].peerid);
+                //xóa người dùng
+                arruserinfo.splice(index, 1);
+                arrsocket.splice(index, 1);
+                io.sockets.emit('DANH_SACH_ONLINE', arruserinfo);
+
+            }
+        }
     });
 })
+//transport close
