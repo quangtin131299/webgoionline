23766e9f69c6a26f1aa7d7197b7914d50141b37c
quangtin131299 <63833652+quangtin131299@users.noreply.github.com>
2020-05-25 17:56:48 +0800

    abc

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 
-const socket = io('https://webgoionline.herokuapp.com/');
+const socket = io('http://192.168.100.4:3000/');
+
+$('#div-chat').hide();
 
 socket.on('DANH_SACH_ONLINE', arruserinfo => {
-    arruserinfo.forEach(user => {
-        let username = user.ten;
-        let id = user.peerid;
-        console.log('<li id="' + id + '">' + username + '</li>');
-        // // $('#uluser').html($('#uluser').html() +'<li id='+id+'>'+username+'</li>');
-
-    });
+        $('#div-chat').show();
+        $('#uluser').html('');
+        for(let i = 0; i < arruserinfo.length; i++){
+            let username = arruserinfo[i].ten;
+            let id = arruserinfo[i].peerid;
+            $('#uluser').html($('#uluser').html() +'<li id='+id+'>'+username+'</li>');        
+        }  
+               
+    
 });
 
-socket.on('CO_NGUOI_MOI', user => {
-    // let username = user.ten;
-    // let id = user.peerid;
-    // // $('#uluser').html($('#uluser').html() +'<li id='+id+'>'+username+'</li>');
+
+socket.on('DANG_KY_THAT_BAI', mess =>{
+    alert(mess);
 });
+
+
 
 // Mở stream
 function openStream() {
-    var config = { audio: false, video: true };
+    var config = { audio: true, video: true };
     return navigator.mediaDevices.getUserMedia(config);
 }
 
@@ -29,17 +34,16 @@ function playStream(idvideo, stream) {
     video.play();
 }
 
-// openStream().then(stream => {playStream('localStream', stream);console.log(stream)});
+var peer = new Peer({host:'peerjs-server.herokuapp.com', secure:true, port:443});
 
-var peer = new Peer({key: 'peerjs', host: 'https://webgoionline.herokuapp.com/', secure: true, port:443});
-
+var idtemp = null;
 peer.on('open', function (id) {
-    console.log(id);
+    idtemp = id;
     $('#myid').html("Your id: " + id);
-    // $('#btnSignup').click(function(){
-    //     const username = $('#textusername').val();
-    //     // socket.emit('NGUOI_DUNG_DANG_KY', {ten: username, peerid: id});
-    // });
+    $('#btnSignup').click(function(){
+        const username = $('#textusername').val();
+        socket.emit('NGUOI_DUNG_DANG_KY', {ten: username, peerid: id});
+    });
 });
 
 //Người gọi
@@ -61,8 +65,12 @@ peer.on('call', call => {
     });
 });
 
-$('#btnSignup').click(function () {
-    const username = $('#textusername').val();
-    socket.emit('NGUOI_DUNG_DANG_KY', {ten: username, peerid: 'id'});
+$('#uluser').on('click','li', function(){
+    const id = $(this).attr('id');
+    console.log(id);
+    openStream().then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remotestream => { playStream('remoteStream', remotestream); });
+    });
 });
-
